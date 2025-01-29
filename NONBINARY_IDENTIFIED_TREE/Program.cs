2f1090435a140ead759e5948b612277018b4743b
rybrrr <kopejskat@gchd.cz>
2025-01-29 15:15:03 +0100

    dhtzheuztehsefdrghr NENAVIDIM TO

---
+++ NONBINARY_IDENTIFIED_TREE/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NONBINARY_IDENTIFIED_TREE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class Node<T>
+        {
+            public T Value { get; set; }
+            public int Key { get; set; }
+
+            public Node<T> LeftChild { get; set; }
+            public Node<T> RightChild { get; set; }
+
+            public Node(int key, T value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+
+        class BinarySearchTree<T>
+        {
+            public Node<T> Root { get; set; }
+
+            public string Show()
+            {
+                void _show(Node<T> node, StringBuilder _sb)
+                {
+                    if (node == null)
+                        return;
+                    
+                    _show(node.LeftChild, _sb);
+                    _show(node.RightChild, _sb);
+                }
+
+                StringBuilder sb = new StringBuilder();
+                return "";
+            }
+        }
+    }
+}
