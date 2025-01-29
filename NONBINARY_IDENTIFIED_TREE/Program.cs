e8c4933b772ee73bd457883b7c10fbdfbc8b91c6
rybrrr <kopejskat@gchd.cz>
2025-01-29 15:55:40 +0100

    rgdrgdrg

--- NONBINARY_IDENTIFIED_TREE/Program.cs
+++ NONBINARY_IDENTIFIED_TREE/Program.cs
@@ -10,7 +10,22 @@ namespace NONBINARY_IDENTIFIED_TREE
     {
         static void Main(string[] args)
         {
+            Node<string> node1 = new Node<string>(1, "Čau");
+            Node<string> node2 = new Node<string>(2, "Zdravim");
+            Node<string> node3 = new Node<string>(3, "Ahoj");
+            Node<string> node4 = new Node<string>(4, "Ahojda");
+            Node<string> node5 = new Node<string>(5, "Ahojky");
 
+            node1.LeftChild = node2;
+            node1.RightChild = node3;
+
+            node3.LeftChild = node4;
+            node3.RightChild = node5;
+
+            BinarySearchTree<string> tree = new BinarySearchTree<string>();
+            tree.Root = node1;
+
+            Console.WriteLine(tree.Show());
         }
 
         class Node<T>
@@ -34,17 +49,44 @@ namespace NONBINARY_IDENTIFIED_TREE
 
             public string Show()
             {
-                void _show(Node<T> node, StringBuilder _sb)
+                StringBuilder sb = new StringBuilder();
+
+                void _show(Node<T> node)
                 {
                     if (node == null)
                         return;
                     
-                    _show(node.LeftChild, _sb);
-                    _show(node.RightChild, _sb);
+                    _show(node.LeftChild);
+                    sb.Append(node.Key.ToString() + " ");
+                    _show(node.RightChild);
                 }
 
-                StringBuilder sb = new StringBuilder();
-                return "";
+                _show(Root);
+                return sb.ToString();
+            }
+
+            public T? Find(int key)
+            {
+                // Using recursion for this kind of task is unnecessarily slow
+
+                Node<T> _find(Node<T> node)
+                {
+                    if (node == null)
+                        return null;
+
+                    if (node.Key == key)
+                        return node;
+                
+                    if (node.Key > key)
+                        return _find(node.LeftChild);
+                    else
+                        return _find(node.RightChild);
+                }
+
+                Node<T>? leaf = _find(Root);
+                T? value = leaf?.Value;
+
+                return value;
             }
         }
     }
